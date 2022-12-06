bc1bd3bad557486e2c68d781194e6a80e5e1686d
Alberto Contreras <alberto.contreras@canonical.com>
2022-12-06 07:48:35 +0100

    di: parse config / policy

--- tools/ds-identify/src/main.rs
+++ tools/ds-identify/src/main.rs
@@ -1,6 +1,6 @@
-#![allow(non_snake_case)]
-
-use std::io::{BufRead, BufReader};
+use std::fmt::Display;
+use std::fs::File;
+use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::path::{self, PathBuf};
 use std::process::{self, Command};
 use std::{
@@ -12,6 +12,8 @@ use std::{
 };
 
 const UNAVAILABLE: &str = "unavailable";
+const DI_ENABLED: &str = "enabled";
+const DI_DISABLED: &str = "disabled";
 
 const PATH_RUN: &str = "run";
 const PATH_SYS_CLASS_DMI_ID: &str = "sys/class/dmi/id";
@@ -28,8 +30,8 @@ const PATH_PROC_UPTIME: &str = "proc/uptime";
 // let PATH_ETC_CI_CFG=  get_env_var("PATH_ETC_CI_CFG:-${PATH_ETC_CLOUD}/cloud.cfg}";
 // let PATH_ETC_CI_CFG_D=  get_env_var("PATH_ETC_CI_CFG_D:-${PATH_ETC_CI_CFG}.d}";
 const PATH_RUN_CI: &str = "cloud-init";
-// let PATH_RUN_CI_CFG=${PATH_RUN_CI_CFG:-${PATH_RUN_CI}/cloud.cfg};
-// let PATH_RUN_DI_RESULT=${PATH_RUN_DI_RESULT:-${PATH_RUN_CI}/.ds-identify.result};
+const PATH_RUN_CI_CFG: &str = "cloud.cfg";
+const PATH_RUN_DI_RESULT: &str = ".ds-identify.result";
 
 const DI_DSLIST_DEFAULT: &str = "MAAS ConfigDrive NoCloud AltCloud Azure Bigstep \
 CloudSigma CloudStack DigitalOcean Vultr AliYun Ec2 GCE OpenNebula OpenStack \
@@ -38,46 +40,88 @@ LXD NWCS";
 
 struct Paths {
     root: PathBuf,
-
+    pub var_lib_cloud: PathBuf,
+    pub di_config: PathBuf,
     pub run: PathBuf,
+    pub proc_cmdline: PathBuf,
     pub proc_1_cmdline: PathBuf,
     pub proc_1_environ: PathBuf,
+    pub proc_uptime: PathBuf,
     pub run_ci: PathBuf,
+    pub run_ci_cfg: PathBuf,
+    pub run_di_result: PathBuf,
 }
 
 impl Paths {
     fn with_root(root: &Path) -> Self {
+        let run = Self::compose_paths(root, PATH_RUN);
+        let run_ci = Self::compose_paths(&run, PATH_RUN_CI);
+        Self::from_roots(root, &run, &run_ci)
+    }
+
+    fn from_roots(root: &Path, run: &Path, run_ci: &Path) -> Self {
         Self {
-            root: PathBuf::from(root),
-            run: Self::compose_paths(root, PATH_RUN),
+            root: root.to_owned(),
+            run: run.to_owned(),
+            var_lib_cloud: Self::compose_paths(root, PATH_VAR_LIB_CLOUD),
+            di_config: Self::compose_paths(root, PATH_DI_CONFIG),
+            proc_cmdline: Self::compose_paths(root, PATH_PROC_CMDLINE),
             proc_1_cmdline: Self::compose_paths(root, PATH_PROC_1_CMDLINE),
             proc_1_environ: Self::compose_paths(root, PATH_PROC_1_ENVIRON),
-            run_ci: Self::compose_paths(root, PATH_RUN_CI),
+            proc_uptime: Self::compose_paths(root, PATH_PROC_UPTIME),
+            run_ci: run_ci.to_owned(),
+            run_ci_cfg: Self::compose_paths(run_ci, PATH_RUN_CI_CFG),
+            run_di_result: Self::compose_paths(run_ci, PATH_RUN_DI_RESULT),
         }
     }
 
-    fn compose_paths<P: AsRef<Path>>(root: P, default: &str) -> PathBuf {
-        root.as_ref().join(default)
+    fn compose_paths<P: AsRef<Path>, S: AsRef<OsStr>>(root: P, default: S) -> PathBuf {
+        root.as_ref().join(default.as_ref())
     }
 
-    fn path_from_env<P: AsRef<Path>>(root: P, name: &str, default: &str) -> PathBuf {
+    fn path_from_env<P: AsRef<Path>, S: AsRef<OsStr>>(root: P, name: &str, default: S) -> PathBuf {
         env::var(name).map_or_else(|_| Self::compose_paths(&root, &default), PathBuf::from)
     }
     pub fn from_env() -> Self {
         let root = env::var("PATH_ROOT").unwrap_or_else(|_| String::from("/"));
         let root = Path::new(&root);
-        let run = Self::path_from_env(root, "PATH_RUN", &PATH_RUN);
-        let proc_1_cmdline = Self::path_from_env(root, "PATH_PROC_1_CMDLINE", &PATH_PROC_1_CMDLINE);
-        let proc_1_environ = Self::path_from_env(root, "PATH_PROC_1_ENVIRON", &PATH_PROC_1_ENVIRON);
-        let run_ci = Self::path_from_env(root, "PATH_RUN_CI", &PATH_RUN_CI);
+        let run = Self::path_from_env(root, "PATH_RUN", Self::compose_paths(&root, &PATH_RUN));
+        let run_ci =
+            Self::path_from_env(root, "PATH_RUN_CI", Self::compose_paths(&run, &PATH_RUN_CI));
+
+        let default_paths = Paths::from_roots(&root, &run, &run_ci);
+
+        let var_lib_cloud =
+            Self::path_from_env(root, "PATH_VAR_LIB_CLOUD", &default_paths.var_lib_cloud);
+        let di_config = Self::path_from_env(root, "PATH_DI_CONFIG", &default_paths.di_config);
+        let proc_cmdline =
+            Self::path_from_env(root, "PATH_PROC_CMDLINE", &default_paths.proc_cmdline);
+        let proc_1_cmdline =
+            Self::path_from_env(root, "PATH_PROC_1_CMDLINE", &default_paths.proc_1_cmdline);
+        let proc_1_environ =
+            Self::path_from_env(root, "PATH_PROC_1_ENVIRON", &default_paths.proc_1_environ);
+        let proc_uptime = Self::path_from_env(root, "PATH_PROC_UPTIME", &default_paths.proc_uptime);
+        let run_ci_cfg = Self::path_from_env(root, "PATH_RUN_CI_CFG", &default_paths.run_ci_cfg);
+        let run_di_result =
+            Self::path_from_env(root, "PATH_RUN_DI_RESULT", &default_paths.run_di_result);
 
         Paths {
             root: PathBuf::from(root),
+            var_lib_cloud,
+            di_config,
             run,
+            proc_cmdline,
             proc_1_cmdline,
             proc_1_environ,
+            proc_uptime,
             run_ci,
+            run_ci_cfg,
+            run_di_result,
         }
+    }
+
+    fn log(&self) -> PathBuf {
+        self.run_ci.join("ds-identify.log")
     }
 }
 
@@ -87,12 +131,13 @@ fn error<S: AsRef<str>>(msg: S) {
     eprintln!("{}", &msg);
 }
 
+// TODO: as macro
 fn debug<S: AsRef<str>>(level: i32, msg: S) {
     // TODO: Find a way to not recompute this value in every call
-    let DI_DEBUG_LEVEL: i32 = get_env_var("DEBUG_LEVEL", String::from("-1"))
+    let debug_level: i32 = get_env_var("DEBUG_LEVEL", String::from("-1"))
         .parse()
         .unwrap();
-    if level >= DI_DEBUG_LEVEL {
+    if level >= debug_level {
         // XXX: enable
         // return;
     }
@@ -113,7 +158,7 @@ struct Info {
     uname_info: UnameInfo,
     virt: Virt,
     pid1_prod_name: String,
-    kernel_cmdline: (),
+    kernel_cmdline: String,
     config: Config,
 }
 
@@ -121,9 +166,9 @@ impl Info {
     fn collect_info(paths: &Paths) -> Self {
         let uname_info = UnameInfo::read();
         let virt = Virt::from(&uname_info);
-        let pid1_prod_name = read_pid1_product_name(&paths.proc_1_environ);
-        let kernel_cmdline = read_kernel_cmdline(&paths, virt.is_container());
-        let config = Config::read();
+        let pid1_prod_name = Self::read_pid1_product_name(&paths.proc_1_environ);
+        let kernel_cmdline = Self::read_kernel_cmdline(&paths, virt.is_container());
+        let config = Config::read(&paths, &kernel_cmdline, &uname_info);
         // read_datasource_list
         // read_dmi_sys_vendor
         // read_dmi_board_name
@@ -172,15 +217,43 @@ impl Info {
 
         string
     }
-}
 
-fn read_kernel_cmdline(paths: &Paths, is_container: bool) {
-    todo!("detect virt first");
-    if is_container {
-        // paths.proc_1_cmdline;
+    fn read_pid1_product_name<T: AsRef<Path>>(proc_pid_1_environ: T) -> String {
+        let product_name = String::from(UNAVAILABLE);
+        let environ = fs::File::open(proc_pid_1_environ.as_ref()).unwrap();
+        let buffered = BufReader::new(environ);
+
+        // /proc/x/environ contain lines null terminated
+        for line in buffered
+            .split(b'\0')
+            .map(|w| String::from_utf8(w.unwrap()).unwrap())
+        {
+            let (key, value) = line.split_once('=').unwrap();
+            if key.to_lowercase() == "product_name" {
+                return value.to_string();
+            }
+        }
+
+        product_name
+    }
+
+    fn read_kernel_cmdline(paths: &Paths, is_container: bool) -> String {
+        if is_container {
+            let cmdline = fs::read_to_string(&paths.proc_1_cmdline).unwrap();
+            let cmdline = cmdline.replace('\0', " ");
+            if cmdline.len() > 0 {
+                return cmdline;
+            }
+            return format!("{UNAVAILABLE}:container");
+        } else if paths.proc_cmdline.is_file() {
+            return fs::read_to_string(&paths.proc_cmdline).unwrap();
+        } else {
+            return format!("{UNAVAILABLE}:no-cmdline");
+        };
     }
 }
 
+#[derive(Debug)]
 struct UnameInfo {
     kernel_name: String,
     node_name: String,
@@ -229,25 +302,6 @@ impl UnameInfo {
             cmd_out: out,
         }
     }
-}
-
-fn read_pid1_product_name<T: AsRef<Path>>(proc_pid_1_environ: T) -> String {
-    let product_name = String::from(UNAVAILABLE);
-    let environ = fs::File::open(proc_pid_1_environ.as_ref()).unwrap();
-    let buffered = BufReader::new(environ);
-
-    // /proc/x/environ contain lines null terminated
-    for line in buffered
-        .split(b'\0')
-        .map(|w| String::from_utf8(w.unwrap()).unwrap())
-    {
-        let (key, value) = line.split_once('=').unwrap();
-        if key.to_lowercase() == "product_name" {
-            return value.to_string();
-        }
-    }
-
-    product_name
 }
 
 fn is_systemd() -> bool {
@@ -325,24 +379,249 @@ impl Virt {
     }
 }
 
-fn read_cmdline() {}
-
+#[derive(Debug)]
 enum Mode {
-    Report,
+    Disabled,
+    Enabled,
     Search,
+    Report,
+}
+
+impl Display for Mode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let cli = match self {
+            Self::Disabled => DI_DISABLED,
+            Self::Enabled => DI_ENABLED,
+            Self::Search => "search",
+            Self::Report => "report",
+        };
+        write!(f, "{cli}")
+    }
+}
+
+#[derive(Debug, Default)]
+enum Found {
+    /// use the first found do no further checking
+    First,
+    /// enable all DS_FOUND
+    #[default]
+    All,
+}
+
+impl Found {
+    fn cli_repr(&self) -> String {
+        match self {
+            Self::First => "first".to_owned(),
+            Self::All => "all".to_owned(),
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+enum Maybe {
+    /// enable all DS_MAYBE
+    #[default]
+    All,
+    /// ignore any DS_MAYBE
+    None,
+}
+
+impl Maybe {
+    fn cli_repr(&self) -> String {
+        match self {
+            Self::None => "none".to_owned(),
+            Self::All => "all".to_owned(),
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+enum NotFound {
+    /// disable cloud-init
+    #[default]
+    Disabled,
+    /// enable cloud-init
+    Enabled,
+}
+
+impl NotFound {
+    fn cli_repr(&self) -> String {
+        match self {
+            Self::Disabled => "disable".to_owned(),
+            Self::Enabled => "enable".to_owned(),
+        }
+    }
+}
+
+// TODO: test fixing default modes
+#[derive(Debug)]
+struct Policy {
+    mode: Mode,
+    on_found: Found,
+    on_maybe: Maybe,
+    on_notfound: NotFound,
+    report: bool,
+}
+
+impl Default for Policy {
+    fn default() -> Self {
+        Self {
+            mode: Mode::Search,
+            on_found: Found::default(),
+            on_maybe: Maybe::default(),
+            on_notfound: NotFound::default(),
+            report: false,
+        }
+    }
+}
+
+impl Policy {
+    fn default_no_dmi() -> Self {
+        Self {
+            mode: Mode::Search,
+            on_found: Found::All,
+            on_maybe: Maybe::All,
+            on_notfound: NotFound::Enabled,
+            ..Default::default()
+        }
+    }
+
+    // XXX: impl From trait
+    fn parse_from_uname(uname: &UnameInfo) -> Self {
+        match &uname.machine[..] {
+            // these have dmi data
+            "i686" | "i386" | "x86_64" => Policy::default(),
+            // aarch64 has dmi, but not currently used (LP: #1663304)
+            "aarch64" | _ => Policy::default_no_dmi(),
+        }
+    }
+
+    fn parse_from_str(policy_str: &str, uname: &UnameInfo) -> Self {
+        let mut policy = Policy::parse_from_uname(&uname);
+
+        let mut mode = None;
+        let mut found = None;
+        let mut maybe = None;
+        let mut notfound = None;
+        for tok in policy_str.trim().split(",") {
+            match tok.split_once('=') {
+                Some(("found", val)) => match val {
+                    "all" => found = Some(Found::All),
+                    "first" => found = Some(Found::First),
+                    val => Self::parse_warn("found", val, &policy.on_found.cli_repr()),
+                },
+                Some(("maybe", val)) => match val {
+                    "all" => maybe = Some(Maybe::All),
+                    "none" => maybe = Some(Maybe::None),
+                    val => Self::parse_warn("maybe", val, &policy.on_maybe.cli_repr()),
+                },
+                Some(("notfound", val)) => match val {
+                    DI_DISABLED => notfound = Some(NotFound::Disabled),
+                    DI_ENABLED => notfound = Some(NotFound::Enabled),
+                    val => Self::parse_warn("notfound", val, &policy.on_notfound.cli_repr()),
+                },
+                Some(_) => continue, // backward compat
+                None => match tok {
+                    DI_ENABLED => mode = Some(Mode::Enabled),
+                    DI_DISABLED => mode = Some(Mode::Disabled),
+                    "search" => mode = Some(Mode::Search),
+                    "report" => mode = Some(Mode::Report),
+                    _ => continue, // backward compat
+                },
+            }
+        }
+
+        if let Some(x) = mode {
+            policy.mode = x;
+        };
+        if let Some(x) = found {
+            policy.on_found = x;
+        };
+        if let Some(x) = maybe {
+            policy.on_maybe = x;
+        };
+        if let Some(x) = notfound {
+            policy.on_notfound = x;
+        };
+
+        policy
+    }
+
+    fn parse_warn(key: &str, invalid: &str, valid: &str) {
+        eprintln!("WARN: invalid value '{invalid}' for key '{key}'. Using {key}={valid}");
+    }
 }
 
 struct Config {
+    dsname: Option<String>,
     mode: Mode,
-    on_found: (),
-    on_maybe: (),
-    on_notfound: (),
-    dsname: (),
+    on_found: Found,
+    on_maybe: Maybe,
+    on_notfound: NotFound,
 }
 
 impl Config {
-    pub fn read() -> Self {
-        todo!();
+    fn from_file(path: &Path) -> (Option<String>, Option<String>) {
+        // TODO: input with explicit keyname
+        if !path.is_file() {
+            panic!("{path:?} exists but is not a file!");
+            // TODO: exit_code 1
+        }
+        let mut dsname = None;
+        let mut policy = None;
+        for line in fs::read_to_string(path).unwrap().lines() {
+            let (key, val) = match line.split_once(':') {
+                None => continue, // no `:` in the line.
+                Some((key, val)) => {
+                    let key = key.trim();
+                    let val = val.trim();
+                    // TODO: unquote `val`
+                    (key, val)
+                }
+            };
+            match key {
+                "datasource" => dsname = Some(val.to_string()),
+                "policy" => policy = Some(val.to_string()),
+                _ => (),
+            };
+        }
+
+        (dsname, policy)
+    }
+
+    pub fn read(paths: &Paths, kernel_cmdline: &str, uname: &UnameInfo) -> Self {
+        let mut dsname = None;
+        let mut policy = None;
+        if paths.di_config.exists() {
+            (dsname, policy) = Self::from_file(&paths.di_config);
+        };
+
+        for tok in kernel_cmdline.split(' ') {
+            match tok.split_once('=') {
+                None => continue,
+                Some((key, val)) => match key {
+                    "ci.ds" | "ci.datasource" => dsname = Some(val.to_string()),
+                    "ci.di.policy" => policy = Some(val.to_string()),
+                    _ => continue,
+                },
+            }
+        }
+
+        let policy = match policy {
+            Some(p) => Policy::parse_from_str(&p, uname),
+            None => Policy::parse_from_uname(uname),
+        };
+
+        // TODO: `debug` policy
+        dbg!(&policy);
+
+        Self {
+            dsname,
+            mode: policy.mode,
+            on_found: policy.on_found,
+            on_maybe: policy.on_maybe,
+            on_notfound: policy.on_notfound,
+        }
     }
 }
 fn read_uptime<P: AsRef<Path>>(path: P) -> String {
@@ -361,6 +640,31 @@ fn read_uptime<P: AsRef<Path>>(path: P) -> String {
     buf.split(' ').take(1).collect()
 }
 
+struct DatasourceList;
+
+impl DatasourceList {
+    fn found() {
+        todo!();
+    }
+}
+
+fn is_manual_clean_and_exiting(var_lib_cloud: &Path) -> bool {
+    return var_lib_cloud.join("instance/manual-clean").is_file();
+}
+
+fn write_result(content: &str, paths: &Paths, mode: &Mode) {
+    // TODO: error handling
+    let mut stream = BufWriter::new(File::open(&paths.run_ci_cfg).unwrap());
+    let mut pre = "";
+    if let Mode::Report = mode {
+        stream.write(b"di_report:\n").unwrap();
+        pre = "  ";
+    }
+    for line in content.lines() {
+        writeln!(stream, "{}{}", pre, line).unwrap();
+    }
+}
+
 fn _main() {
     // TODO: ensure_sane_path
 
@@ -368,31 +672,66 @@ fn _main() {
     let args_str: &str = &args[1..].join(" ");
 
     let paths = Paths::from_env();
-
-    let DI_LOG = paths.run_ci.join("ds-identify.log");
+    let di_log = paths.log();
 
     debug(
         1,
         format!(
             "[up {}s] ds-identify {args_str}",
-            read_uptime(PATH_PROC_UPTIME)
+            read_uptime(&paths.proc_uptime)
         ),
     );
 
     let info = Info::collect_info(&paths);
 
-    if DI_LOG.to_str().unwrap() == "stderr" {
+    if di_log.to_str().unwrap() == "stderr" {
         todo!();
     } else {
-        todo!("print to `DI_LOG`");
         let old_cli_str = info.to_old_str();
+        // TODO: print to `DI_LOG`;
         println!("{old_cli_str}");
+    }
+
+    const RET_DISABLED: i32 = 1;
+    const RET_ENABLED: i32 = 0;
+
+    match info.config.mode {
+        Mode::Disabled => {
+            debug(
+                1,
+                format!("mode={}. returning {}", Mode::Disabled, RET_DISABLED),
+            );
+            process::exit(RET_DISABLED);
+        }
+        Mode::Enabled => {
+            debug(
+                1,
+                format!("mode={}. returning {}", Mode::Enabled, RET_ENABLED),
+            );
+            process::exit(RET_ENABLED);
+        }
+        _ => (),
+    }
+
+    if let Some(dsname) = info.config.dsname {
+        debug(1, format!("datasource '{dsname}' specified."));
+        // TODO: found
+        return;
+    }
+
+    if is_manual_clean_and_exiting(&paths.var_lib_cloud) {
+        debug(
+            1,
+            "manual_cache_clean enabled. Not writing datasource_list.",
+        );
+        write_result("# manual_cache_clean.", &paths, &info.config.mode);
+        return;
     }
 }
 
 fn main() {
-    let DI_MAIN = get_env_var("DI_MAIN", String::from("main"));
-    match &DI_MAIN[..] {
+    let di_main = get_env_var("DI_MAIN", String::from("main"));
+    match &di_main[..] {
         "main" | "print_info" | "noop" => _main(),
         _ => {
             error("unexpected value for DI_MAIN");
