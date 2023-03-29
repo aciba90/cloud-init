8343652a069716dd271f9848880c30b7e16f4ee3
Alberto Contreras <alberto.contreras@canonical.com>
2023-03-29 09:24:28 +0200

    format

--- extract.py
+++ extract.py
@@ -13,9 +13,10 @@
 2023-03-27 15:49:06,560 - subp.py[DEBUG]: Running command ['netplan', 'generate'] with allowed return codes [0] (shell=False, capture=True)
 2023-03-27 15:49:06,890 - subp.py[DEBUG]: Running command ['udevadm', 'test-builtin', 'net_setup_link', '/sys/class/net/ens4'] with allowed return codes [0] (shell=False, capture=True)
 """
-from pathlib import Path
-import tarfile
 import datetime as dt
+import tarfile
+from pathlib import Path
+
 import pandas as pd
 
 INDIR = Path("./tmp/output")
@@ -25,11 +26,11 @@ def extract_systemd_blame(txt: bytes):
     """
     Extracts time from:
 
-      2.539s cloud-init-local.service     
+      2.539s cloud-init-local.service
     """
     for line in txt.splitlines():
         if b"cloud-init-local.service" in line:
-            return float(line.strip().split(b' ')[0][:-1])
+            return float(line.strip().split(b" ")[0][:-1])
     return None
 
 
@@ -45,7 +46,7 @@ def extract_cloud_init_log(txt: bytes):
             if b"finish: init-local: SUCCESS" in line:
                 break
         return float(lines[idx + 1].split(b" ")[-3])
-    
+
     local_time = extract_local_time()
 
     def extract_netplan_time():
@@ -58,13 +59,15 @@ def extract_cloud_init_log(txt: bytes):
         for idx, line in enumerate(lines):
             if b"Running command ['netplan', 'generate']" in line:
                 break
-        
-        ex_time = lambda ll: dt.datetime.strptime(ll.split(b" ")[1].decode(), "%H:%M:%S,%f")
+
+        ex_time = lambda ll: dt.datetime.strptime(
+            ll.split(b" ")[1].decode(), "%H:%M:%S,%f"
+        )
         t_0 = ex_time(line)
         t_1 = ex_time(lines[idx - 1])  # substraction as the order is reversed
 
         return (t_1 - t_0).total_seconds()
-    
+
     netplan_time = extract_netplan_time()
     return local_time, netplan_time
 
@@ -74,28 +77,39 @@ def main():
     for release_path in INDIR.glob("*/"):
         release = release_path.name
         for boot_info_path in release_path.glob("*"):
-            timestamp, base_or_update, boot_number, *_ = str(boot_info_path.name).split("-")
+            timestamp, base_or_update, boot_number, *_ = str(
+                boot_info_path.name
+            ).split("-")
             row = dict()
             row["release"] = release
             row["timestamp"] = timestamp
             row["base_or_update"] = base_or_update
             with tarfile.open(boot_info_path) as boot_info_tar:
-                systemd_blame = boot_info_tar.extractfile("./systemd_blame.txt").read()
+                systemd_blame = boot_info_tar.extractfile(
+                    "./systemd_blame.txt"
+                ).read()
                 systemd_local_time = extract_systemd_blame(systemd_blame)
                 row["systemd_local_time"] = systemd_local_time
                 # TODO: store local_time
                 if "1s" == boot_number:
                     continue
                 print(boot_info_path)
-                with tarfile.open(fileobj=boot_info_tar.extractfile("./cloud-init.tar.gz")) as cloud_init_logs:
-                    cloud_init_log = cloud_init_logs.extractfile("cloud-init-logs-2023-03-27/cloud-init.log").read()
-                    local_time, netplan_time = extract_cloud_init_log(cloud_init_log)
+                with tarfile.open(
+                    fileobj=boot_info_tar.extractfile("./cloud-init.tar.gz")
+                ) as cloud_init_logs:
+                    cloud_init_log = cloud_init_logs.extractfile(
+                        "cloud-init-logs-2023-03-27/cloud-init.log"
+                    ).read()
+                    local_time, netplan_time = extract_cloud_init_log(
+                        cloud_init_log
+                    )
                     row["local_time"] = local_time
                     row["netplan_time"] = netplan_time
             data.append(row)
 
     df = pd.DataFrame(data)
     df.to_csv(INDIR / "data.csv", index=False)
+
 
 if __name__ == "__main__":
     main()
